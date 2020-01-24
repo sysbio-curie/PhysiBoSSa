8571cf992c6a7e23db77b517db63b68a2205dd41
Gerard Pradas <gerard.pradas@bsc.es>
2020-01-24 11:16:23 +0100

    excluded get time for next maboss from critical

--- sample_projects/spheroid_TNF/custom_modules/custom.cpp
+++ sample_projects/spheroid_TNF/custom_modules/custom.cpp
@@ -185,11 +185,10 @@ void boolean_network_rule(Cell* pCell, Phenotype& phenotype, double dt )
 		set_input_nodes(pCell);
 
 		#pragma omp critical
-		{
-			pCell->maboss_cycle_network->run_maboss();
-			double next_run_in = pCell->maboss_cycle_network->get_time_to_update();
-			pCell->custom_data["next_physibossa_run"] = PhysiCell_globals.current_time + next_run_in;
-		}
+		pCell->maboss_cycle_network->run_maboss();
+		
+		double next_run_in = pCell->maboss_cycle_network->get_time_to_update();
+		pCell->custom_data["next_physibossa_run"] = PhysiCell_globals.current_time + next_run_in;
 		
 		from_nodes_to_cell(pCell, phenotype, dt);
 	}
