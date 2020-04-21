a1836bfc9bddef3e85d561fd2bd8c65485ac6f9b
Vincent Noël <contact@vincent-noel.fr>
2020-04-21 12:55:10 +0200

    Reverting PRNG output

--- core/PhysiCell_utilities.cpp
+++ core/PhysiCell_utilities.cpp
@@ -94,9 +94,7 @@ long SeedRandom( void )
 
 double UniformRandom()
 {
-	double t_rand = std::generate_canonical<double, 10>(gen);
-	std::cout << "Random number : " << t_rand << std::endl;
-	return t_rand;
+	return std::generate_canonical<double, 10>(gen);
 }
 
 double NormalRandom( double mean, double standard_deviation )
