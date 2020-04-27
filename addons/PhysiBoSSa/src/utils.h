910f8f2f937ee303cde9dc5566feeb6fb6c2da72
Vincent Noël <contact@vincent-noel.fr>
2020-04-27 13:50:53 +0200

    Now using PhysiCell PRNG in PhysiBoSS addon
    Co-authored-by: marcorusc <m.ruscone94@gmail.com>

--- addons/PhysiBoSSa/src/utils.h
+++ addons/PhysiBoSSa/src/utils.h
@@ -1,25 +1,4 @@
 #ifndef _PhysiBoSSa_utils_h_
 #define _PhysiBoSSa_utils_h_
 
-#include <ctime>
-#include <random>
-
-/**
- * \brief Generate a random integer
- * \return An integer
- */
-int UniformInt();
-
-/**
- * \brief Generate a random float
- * \return A float
- */
-double UniformRandom( void );
-
-/**
- * \brief Generate a random float
- * \return A float
- */
-double UniformRandom11( void );
-
 #endif
