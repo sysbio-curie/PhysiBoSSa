70511f4bf0516f716992ba6592f191283463ac0d
Vincent Noël <contact@vincent-noel.fr>
2020-04-27 13:50:53 +0200

    Now using PhysiCell PRNG in PhysiBoSS addon
    Co-authored-by: marcorusc <m.ruscone94@gmail.com>

--- addons/PhysiBoSSa/src/boolean_network.h
+++ addons/PhysiBoSSa/src/boolean_network.h
@@ -2,6 +2,7 @@
 #define __CELL_CYCLE_NETWORK_H
 
 #include "maboss_network.h"
+#include "../../../core/PhysiCell_utilities.h"
 
 /**
  * \class BooleanNetwork
@@ -28,7 +29,7 @@ class BooleanNetwork
 		/** \brief choose a random update time, to asynchronize it between all cells 
 		 *
 		 * Set the next time at which to update the current cell's network. The time in between two udpates is chosen randomly in order to not update all cells together. */
-		inline void set_time_to_update(){this->time_to_update = (1 + 0.5*UniformRandom11()) * this->maboss.get_update_time_step();}
+		inline void set_time_to_update(){this->time_to_update = (PhysiCell::UniformRandom()+0.5) * this->maboss.get_update_time_step();}
 
 	public:
 		/** \brief Initialize a maboos network */
