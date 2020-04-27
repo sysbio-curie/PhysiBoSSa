b6836ed0d0baa5d70a17a5e5b1802fda2cf3a0d4
Vincent Noël <contact@vincent-noel.fr>
2020-04-27 12:46:00 +0200

    Adding an function for the PRNG to return an int
    Co-authored-by: marcorusc <m.ruscone94@gmail.com>

--- core/PhysiCell_utilities.h
+++ core/PhysiCell_utilities.h
@@ -81,6 +81,7 @@ namespace PhysiCell{
 long SeedRandom( long input );
 long SeedRandom( void );
 double UniformRandom( void );
+int UniformInt();
 double NormalRandom( double mean, double standard_deviation );
 double dist_squared(std::vector<double> p1, std::vector<double> p2);
 double dist(std::vector<double> p1, std::vector<double> p2);
