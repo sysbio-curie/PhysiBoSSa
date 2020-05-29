df7e1077dbd2adfaaff77d1e780c0647ba928a1a
Vincent Noël <contact@vincent-noel.fr>
2020-05-29 20:25:02 +0200

    simulation engine now a class variable

--- addons/PhysiBoSSa/src/maboss_network.cpp
+++ addons/PhysiBoSSa/src/maboss_network.cpp
@@ -21,7 +21,8 @@ void MaBoSSNetwork::init_maboss( std::string networkFile, std::string configFile
 
 	IStateGroup::checkAndComplete(this->network);
 
-	// Initialize the map relation between node name and index positions
+	engine = new StochasticSimulationEngine(this->network, this->config, PhysiCell::UniformInt());
+
 	int i = 0;
 	std::vector<Node *> nodes = this->network->getNodes();
 	for (auto node : nodes)
@@ -34,6 +35,8 @@ void MaBoSSNetwork::init_maboss( std::string networkFile, std::string configFile
 /** Default estructor */
 void MaBoSSNetwork::delete_maboss()
 {
+	delete this->engine;
+	this->engine = NULL;
 	delete this->network;
 	this->network = NULL;
 	delete this->config;
@@ -116,11 +119,7 @@ void MaBoSSNetwork::run_simulation(std::vector<bool>* node_values)
 {	
 	NetworkState_Impl state = this->create_networkstate(node_values);
 
-	// Engine created for a single isolated simulation
-	StochasticSimulationEngine* engine = new StochasticSimulationEngine(this->network, this->config);
-	engine->setSeed(PhysiCell::UniformInt());
 	state = engine->run(&state, NULL);
-	delete engine;
 
 	this->retrieve_networkstate_values(state, node_values);
 }
