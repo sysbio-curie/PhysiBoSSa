41629011a02a695ca1c7e8691ab4779c664adc08
Gerard Pradas <pradasgsj@gmail.com>
2020-06-08 13:17:28 +0200

    add doxyfile comments

--- addons/PhysiBoSSa/src/maboss_network.h
+++ addons/PhysiBoSSa/src/maboss_network.h
@@ -1,21 +1,21 @@
 #ifndef _MaBoSS_Net_h_
 #define _MaBoSS_Net_h_
 
-/**
- * \brief Interface with MaBoSS software
- *
- * One network object contains network configs.
- * Handle initialization, running it...
- *
- * Created on 05/19/2017
- * G. Letort, Institute Curie
- */
-
 #include "StochasticSimulationEngine.h"
 #include "BooleanNetwork.h"
 #include "RunConfig.h"
 #include "utils.h"
 
+/**
+ *	\class MaBoSSNetwork
+ *	\brief Interface with MaBoSS software
+ * 
+ *	\details The MaBOSS network class contains network configs to handel the initialization and run the simulations given an input.
+ *
+ *	\date 06/08/2020
+ *	\author Gaelle Letort, Institut Curie
+ *	\author Gerard Pradas, BSC-CNS
+ */
 class MaBoSSNetwork
 {
 	private:
@@ -24,45 +24,71 @@ class MaBoSSNetwork
 		/** \brief MaBoSS instances: configurations */
 		RunConfig* config;
 
-		/** \brief Time step to update the cycle */
-		double update_time_step = 12;
+		/** \brief Time step between each MaBoSS simulation */
+		double update_time_step = 10;
 
 		/** \brief Names and indices of network nodes */
 		std::map< std::string, int > node_names;
 
-		/** \brief Given a vectors of bools, returns the corresponding state */
-		NetworkState_Impl load_state(std::vector<bool>* input);
+		/**
+		 * \brief Given a vectors of bools, returns the corresponding state 
+		 * \param input Vector of bools with the same size as the network
+		 * \return A NetworkState_Impl instance with the input values
+		 */
+		NetworkState_Impl create_networkstate(std::vector<bool>* input);
 
-		/** \brief Given a state and a vectors of bools, writes the states into the vector */
-		void recover_state(NetworkState_Impl state, std::vector<bool>* output);
+		/**
+		 * \brief Given a state and a vectors of bools, writes the states into the vector
+		 * \param state State to retrieve the boolean values
+		 * \param output Vector of bools to store the state 
+		 */
+		void retrieve_networkstate_values(NetworkState_Impl state, std::vector<bool>* output);
 
 	public:
-		/** \brief Initialize network */
+		/** 
+		 * \brief Class initializer 
+		 * \param networkFile,configFile MaBoSS configutation files
+		 */
 		void init_maboss( std::string networkFile, std::string configFile);
 
-		/** \brief Destructor */
+		/** \brief Class destructor */
 		void delete_maboss();
 
-		/** \brief Restart a vector of bools, to the init values of the network */
+		/** 
+		 * \brief Restart a vector of bools, to the init values of the network 
+		 * \param node_values Vector of bools to write an initial state of the network
+		 */
 		void restart_node_values(std::vector<bool>* node_values);
 
-		/** \brief Run the current network*/
+		/** 
+		 * \brief Run the current network
+		 * \param node_values Vector mapping a boolean network to run a simulation
+		 */
 		void run_simulation(std::vector<bool>* node_values);
 
-		/** \brief Return node of given name current value
-		 *
-		 * Return -1 if node doesn't exit \n
-		 * Return 0 if node is 0 \n
-		 * Return 1 if node is 1 */
+		/** 
+		 * \brief Return node of given name current value
+		 * \param name Node name existing in the boolean network
+		 * \return -1 if node doesn't exit, 0 if node is 0, 1 if node is 1
+		 */
 		int get_maboss_node_index( std::string name );
 
-		/** \brief Return update time value */
+		/** 
+		 * \brief Get update time step 
+		 * \return Time step between each MaBoSS simulation
+		 */
 		inline double get_update_time_step(){ return this->update_time_step; }
 
-		/** \brief Setter update time */
+		/**
+		 * \brief Set update time step 
+		 * \param time_step Time step between each MaBoSS simulation
+		 */
 		inline void set_update_time_step(double time_step) { this->update_time_step = time_step;}
 		
-		/** \brief Print current state of all the nodes of the network */
+		/** 
+		 * \brief Print current state of all the nodes of the network 
+		 * \param node_values Boolean vector mapping a boolean network
+		 */
 		void print_nodes(std::vector<bool>* node_values);
 };
 
--- addons/PhysiBoSSa/src/utils.cpp
+++ addons/PhysiBoSSa/src/utils.cpp
@@ -3,17 +3,20 @@
 std::random_device rd;
 std::mt19937 gen(rd());
 
+/* Generate a random integer */
 int UniformInt()
 {
 	std::uniform_int_distribution<int> int_dis;
 	return int_dis(gen);
 }
 
+/* Generate a random float */
 double UniformRandom()
 {
 	return std::generate_canonical<double, 10>(gen);
 }
 
+/* Generate a random float */
 double UniformRandom11()
 {
 	double res = UniformRandom();
