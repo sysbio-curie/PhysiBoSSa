a013625486e3e2de1fda51fd70fe73b03b023f31
Vincent Noël <contact@vincent-noel.fr>
2021-05-30 17:20:28 +0200

    Building libMaBoSS with conda

---
+++ libmaboss/build.sh
@@ -0,0 +1,20 @@
+cd addons/PhysiBoSS/MaBoSS-env-2.0/engine/src
+
+make install_lib
+make MAXNODES=128 install_lib
+make MAXNODES=256 install_lib
+make MAXNODES=512 install_lib
+make MAXNODES=1024 install_lib
+
+make install_alib
+make MAXNODES=128 install_alib
+make MAXNODES=256 install_alib
+make MAXNODES=512 install_alib
+make MAXNODES=1024 install_alib
+
+mkdir -p ${PREFIX}/lib
+mv ../pub/libMaBoSS* ${PREFIX}/lib
+
+mkdir -p ${PREFIX}/include
+mv *.h ${PREFIX}/include
+
